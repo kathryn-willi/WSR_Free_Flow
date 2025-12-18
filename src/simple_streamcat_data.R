60e8c19479fa42b100c551c9bde963422e09c0c7
kathryn-willi <kathryn.willi@colostate.edu>
2025-12-18 13:56:34 -0500

    watershed stat functions

---
+++ src/simple_streamcat_data.R
@@ -0,0 +1,15 @@
+simple_streamcat_data <- function(df){
+
+streamcat_vars <- StreamCatTools::sc_get_data(metric = paste(selected_vars, collapse = ","),
+                                              aoi = 'watershed',
+                                              showAreaSqKm = TRUE,
+                                              comid = df$comid) %>%
+  # remove variables we don't particularly care about that get returned:
+  select(-contains("areasqkm"))
+
+streamcat_list_simple <- df %>%
+  select(index, comid) %>%
+  left_join(., streamcat_vars, by = "comid")
+
+  return(streamcat_list_simple)
+}
